9c4a57df393794de52621c18825705c91b2bb8dc
Hector Veas <hveas10@gmail.com>
2021-06-06 03:50:22 -0400

    feat: documentation swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('APPointVet')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   app.useGlobalPipes(new ValidationPipe({ forbidNonWhitelisted: true }));
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
