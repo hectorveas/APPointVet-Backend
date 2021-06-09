593cb4c0c45772cab83c4c8ce54eecf41c888d35
Hector Veas <hveas10@gmail.com>
2021-06-09 14:56:23 -0400

    fix: login

--- src/auth/strategies/specialist.strategy.ts
+++ src/auth/strategies/specialist.strategy.ts
@@ -5,7 +5,10 @@ import { AuthService } from '../services/auth.service';
 import { Strategy } from 'passport-local';
 
 @Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
+export class SpecialistStrategy extends PassportStrategy(
+  Strategy,
+  'specialist',
+) {
   constructor(private authService: AuthService) {
     super({
       usernameField: 'email',
@@ -16,20 +19,8 @@ export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
   async validate(mail: string, password: string) {
     const user = await this.authService.validateSpecialist(mail, password);
     if (!user) {
-      const user = await this.authService.validatePetOwner(mail, password);
-      if (!user) {
-        throw new UnauthorizedException('not allow');
-      }
-      return user;
-    }
-    return user;
-  }
-
-  async validate1(mail: string, password: string) {
-    const petOwner = await this.authService.validatePetOwner(mail, password);
-    if (!petOwner) {
       throw new UnauthorizedException('not allow');
     }
-    return petOwner;
+    return user;
   }
 }
